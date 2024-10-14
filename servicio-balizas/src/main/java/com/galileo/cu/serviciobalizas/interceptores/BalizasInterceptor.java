1a119e1ff95d2c314b8d420eb3a42ae40d063056
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-14 01:51:26 -0400

    Eliminando Logs Innecesarios

--- servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/interceptores/BalizasInterceptor.java
+++ servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/interceptores/BalizasInterceptor.java
@@ -1,9 +1,7 @@
 package com.galileo.cu.serviciobalizas.interceptores;
 
-
 import com.galileo.cu.commons.models.dto.JwtObjectMap;
 import com.galileo.cu.serviciobalizas.repositorio.BalizasRepository;
-
 
 import java.io.IOException;
 
@@ -23,56 +21,50 @@ import com.google.common.base.Strings;
 public class BalizasInterceptor implements HandlerInterceptor {
 
 	@Autowired
-    private ObjectMapper objectMapper;
-	
+	private ObjectMapper objectMapper;
+
 	@Autowired
 	private BalizasRepository baliza;
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws ServletException, IOException {
-		System.out.println("INTERCEPTOR**********************");
-		System.out.println(request.getHeader("Authorization"));
+
 		if (request.getMethod().equals("GET")) {
 			if (!Strings.isNullOrEmpty(request.getHeader("Authorization"))) {
 				String token = request.getHeader("Authorization").replace("Bearer ", "");
-				System.out.println(token.toString());
-				
+
 				try {
 					String[] chunks = token.split("\\.");
 					Base64.Decoder decoder = Base64.getUrlDecoder();
 					String header = new String(decoder.decode(chunks[0]));
 					String payload = new String(decoder.decode(chunks[1]));
 
-					System.out.println(payload.toString());
-
 					JwtObjectMap jwtObjectMap = objectMapper.readValue(payload.toString().replace("Perfil", "perfil"),
 							JwtObjectMap.class);
-					System.out.println(jwtObjectMap.getId());
 
-					System.out.println("Path:" + request.getRequestURI());
-					System.out.println("Descripcion:" + jwtObjectMap.getPerfil().getDescripcion());
 					if ((request.getRequestURI().equals("/balizas/search/buscarBalizas")
 							|| request.getRequestURI().equals("/balizas/search/filtrarObjetivo")
 							|| request.getRequestURI().equals("/balizas/search/filtro"))
 							&& (jwtObjectMap.getPerfil().getDescripcion().equals("Usuario Final")
 									|| jwtObjectMap.getPerfil().getDescripcion().equals("Invitado Externo"))) {
-						System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-						System.out.println("id parametro: " + request.getParameter("idAuth"));
+
 						if (jwtObjectMap.getId().equals(request.getParameter("idAuth"))) {
 							return true;
 						} else {
 							return true;
-							/*System.out.println("EL USUARIO ENVIADO NO COINCIDE CON EL AUTENTICADO");
-							response.resetBuffer();
-							response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-							response.setHeader("Content-Type", "application/json;charset=UTF-8");
-							response.getOutputStream()
-									.write("{\"errorMessage\":\"EL USUARIO ENVIADO NO COINCIDE CON EL AUTENTICADO!\"}"
-											.getBytes("UTF-8"));
-							response.flushBuffer();
-
-							return false;*/
+							/*
+							 * System.out.println("EL USUARIO ENVIADO NO COINCIDE CON EL AUTENTICADO");
+							 * response.resetBuffer();
+							 * response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+							 * response.setHeader("Content-Type", "application/json;charset=UTF-8");
+							 * response.getOutputStream()
+							 * .write("{\"errorMessage\":\"EL USUARIO ENVIADO NO COINCIDE CON EL AUTENTICADO!\"}"
+							 * .getBytes("UTF-8"));
+							 * response.flushBuffer();
+							 * 
+							 * return false;
+							 */
 						}
 					}
 				} catch (Exception e) {
@@ -88,17 +80,21 @@ public class BalizasInterceptor implements HandlerInterceptor {
 				}
 
 			} else {
-				/*System.out.println("NO HAY TOKEN");
-				response.resetBuffer();
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.setHeader("Content-Type", "application/json;charset=UTF-8");
-				String s="{\"errorMessage\":\"Necesita enviar un Token Válido "+request.getMethod()+" Servicio-Objetivos!\"}";
-				response.getOutputStream().write(s.getBytes("UTF-8"));
-				response.flushBuffer();*/
+				/*
+				 * System.out.println("NO HAY TOKEN");
+				 * response.resetBuffer();
+				 * response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				 * response.setHeader("Content-Type", "application/json;charset=UTF-8");
+				 * String
+				 * s="{\"errorMessage\":\"Necesita enviar un Token Válido "+request.getMethod()
+				 * +" Servicio-Objetivos!\"}";
+				 * response.getOutputStream().write(s.getBytes("UTF-8"));
+				 * response.flushBuffer();
+				 */
 
 				return true;
 			}
 		}
 		return true;
-	}	
+	}
 }
--- servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/interceptores/ValidateAuthorization.java
+++ servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/interceptores/ValidateAuthorization.java
@@ -20,45 +20,37 @@ import lombok.Setter;
 @NoArgsConstructor
 @Data
 public class ValidateAuthorization {
-    public ObjectMapper objectMapper;    
+    public ObjectMapper objectMapper;
     public HttpServletRequest req;
     public JwtObjectMap jwtObjectMap;
-    
+
     public boolean Validate() {
-        if (req==null) {
+        if (req == null) {
             throw new RuntimeException("Error Validando Autorización la Petición no debe ser Nula ");
         }
-        System.out.println("ValidateAuthorization**********************");
-		System.out.println(req.getHeader("Authorization"));
-        System.out.println("METHOD: "+req.getMethod());
 
         if (!Strings.isNullOrEmpty(req.getHeader("Authorization"))) {
             String token = req.getHeader("Authorization").replace("Bearer ", "");
-            System.out.println(token.toString());
+
             try {
                 String[] chunks = token.split("\\.");
                 Base64.Decoder decoder = Base64.getUrlDecoder();
                 String header = new String(decoder.decode(chunks[0]));
                 String payload = new String(decoder.decode(chunks[1]));
 
-                System.out.println(payload.toString());
-
                 jwtObjectMap = objectMapper.readValue(payload.toString().replace("Perfil", "perfil"),
-							JwtObjectMap.class);
-                System.out.println(jwtObjectMap.getId());
+                        JwtObjectMap.class);
 
-                System.out.println("Path:" + req.getRequestURI());
-                System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-                System.out.println("id parametro: " + req.getParameter("idAuth"));
-                if (!Strings.isNullOrEmpty(req.getParameter("idAuth")) && jwtObjectMap.getId().equals(req.getParameter("idAuth"))) {
+                if (!Strings.isNullOrEmpty(req.getParameter("idAuth"))
+                        && jwtObjectMap.getId().equals(req.getParameter("idAuth"))) {
                     return true;
-                }else if(Strings.isNullOrEmpty(req.getParameter("idAuth")) && !req.getMethod().equals("GET")){
+                } else if (Strings.isNullOrEmpty(req.getParameter("idAuth")) && !req.getMethod().equals("GET")) {
                     return true;
-                } else{
+                } else {
                     return false;
                 }
             } catch (Exception e) {
-                System.out.println("ERROR, Validando Autorización: "+e.getMessage());
+                System.out.println("ERROR, Validando Autorización: " + e.getMessage());
                 throw new RuntimeException("ERROR, Validando Autorización ");
             }
         } else {
