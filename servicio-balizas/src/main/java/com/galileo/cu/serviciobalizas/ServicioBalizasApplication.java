94c801e63eb1b1ac5549b932209dd31f5e5d68a0
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-05 15:39:04 -0500

    mapAddress Recompilando

--- servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/ServicioBalizasApplication.java
+++ servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/ServicioBalizasApplication.java
@@ -20,7 +20,7 @@ public class ServicioBalizasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Balizas V-1.1.1 24-10-30 15:47");
+		System.out.println("Balizas V-1.1.1 24-11-05 15:38");
 
 	}
 
