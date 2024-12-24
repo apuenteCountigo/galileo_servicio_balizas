ecf3bd2d69ee0f0d965bc24929448553abd095a8
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-24 12:49:50 -0500

    V-1.1.4

--- servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/ServicioBalizasApplication.java
+++ servicio-balizas/src/main/java/com/galileo/cu/serviciobalizas/ServicioBalizasApplication.java
@@ -20,7 +20,7 @@ public class ServicioBalizasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Balizas V-1.1.2 24-11-05 16:10");
+		System.out.println("Balizas V-1.1.4 24-12-24 12:49");
 
 	}
 
